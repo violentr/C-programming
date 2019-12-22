506820280d9d0b8b74d8e717acc1098a8d47e6fa
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-12-22 16:41:44 +0000

    Add simple implementation of HTTP client

---
+++ playground/Sockets/HTTP/http_client.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+/*
+ * This example works om Mac
+ */
+
+int main (int argc, char *argv[]){
+  char *address;
+  address = argv[1];
+
+  int client_socket;
+  client_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+  struct sockaddr_in remote_address;
+  remote_address.sin_family = AF_INET;
+  remote_address.sin_port = htons(80);
+
+  // https://linux.die.net/man/3/inet_aton
+  inet_aton(address, &remote_address.sin_addr);
+  connect(client_socket, (struct sockaddr *)&remote_address, sizeof(remote_address));
+
+  char request[] = "GET / HTTP/1.1 \r\n\r\n";
+  char response[4096];
+  send(client_socket, request, sizeof(request), 0);
+  recv(client_socket, &response, sizeof(response), 0);
+  printf("Response received:\n %s\n", response);
+  close(client_socket);
+
+  return 0;
+}
