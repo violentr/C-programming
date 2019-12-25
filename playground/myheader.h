5876f0e2f6d512fd556db608dc2973a61fcb250f
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-12-25 15:02:04 +0000

    Add not_string, oposite to substr search
    This is opposite to substring, finds everything
    a part from the search string

--- playground/myheader.h
+++ playground/myheader.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <string.h>
+#include <stdlib.h>
 void printFactorial(int num){
   int place = num;
   int result = 1;
@@ -10,3 +11,24 @@ void printFactorial(int num){
   }
   printf("The factorial of number %d is %d\n", num, result);
 }
+
+/* Extract everything a part from the search string*/
+char *not_substr(char buff[], char *search_string){
+
+ int buff_size = (int)strlen(buff);
+ int length = (int)strlen(search_string);
+ char * result = (char *)malloc(sizeof(char)*length);
+ int DEBUG = 0;
+
+ int step = length + 1;
+ int index;
+ for(int i = step; i < buff_size;  i++) {
+  index = i - step;
+  result[index] = buff[i];
+  if (DEBUG){
+    printf("Result[%d]=%c\n", i, buff[i]);
+  }
+ }
+ return result;
+}
+
